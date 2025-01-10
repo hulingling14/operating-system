fb9853096a89bb39ff2ca97c13f4921cb4c8d570
hulinglingxiaobao <3239197769@qq.com>
2025-01-10 10:49:12 +0800

    2025-1-10 使用匿名内部类创建Runnable子类并重写run

---
+++ Demo4.java
@@ -0,0 +1,23 @@
+public class Demo4 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("hello Thread");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t.start();
+
+        while (true) {
+            System.out.println("hello main");
+            Thread.sleep(1000);
+        }
+    }
+}
