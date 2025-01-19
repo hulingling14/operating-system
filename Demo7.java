6a41a9ac4725fc2b686e4575c5afa1eef179c55c
hulinglingxiaobao <3239197769@qq.com>
2025-01-19 19:15:11 +0800

    2025-1-19 创建目录

--- Demo7.java
+++ Demo7.java
@@ -1,22 +1,9 @@
+import java.io.File;
+
 public class Demo7 {
-    public static void main(String[] args) throws InterruptedException {
-        Thread t = new Thread(() -> {
-            while (true) {
-                System.out.println("hello Thread");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        t.start();
-
-        // 获取一下线程的这些属性
-        System.out.println("线程id：" + t.getId());
-        System.out.println("线程名字：" + t.getName());
-        System.out.println("线程状态：" + t.getState());
-        System.out.println("线程优先级：" + t.getPriority());
+    public static void main(String[] args) {
+        File f = new File("./abc/def/ghi");
+        boolean ok = f.mkdirs();
+        System.out.println(ok);
     }
 }
