1705b6873f65ca17f1fc90b9a17166c1a775b64c
hulinglingxiaobao <3239197769@qq.com>
2025-01-19 17:28:04 +0800

    2025-1-19 普通删除

--- Demo3.java
+++ Demo3.java
@@ -1,23 +1,9 @@
-public class Demo3 {
-    public static void main(String[] args) throws InterruptedException {
-        Thread t=new Thread(){
-            @Override
-            public void run() {
-                while(true){
-                    System.out.println("hello Thread");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        };
-        t.start();
+import java.io.File;
 
-        while(true){
-            System.out.println("hello main");
-            Thread.sleep(1000);
-        }
+public class Demo3 {
+    public static void main(String[] args) {
+        File file = new File("./Lisen.js");
+        boolean ok = file.delete();
+        System.out.println(ok);
     }
 }
