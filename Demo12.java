75ef8872eeb35b8001e6621dc882185785c2015d
hulinglingxiaobao <3239197769@qq.com>
2025-01-19 21:47:52 +0800

    2025-1-19 写入

--- Demo12.java
+++ Demo12.java
@@ -1,24 +1,14 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class Demo12 {
-    private static boolean isQuit = false;
-
-    public static void main(String[] args) throws InterruptedException {
-        Thread t = new Thread(() -> {
-            while (!isQuit) {
-                System.out.println("hello Thread");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("t 线程执行结束");
-        });
-
-        t.start();
-
-        Thread.sleep(2000);
-        //修改 isQuit 变量，就能够影响到 t 线程结束了
-        System.out.println("main 线程尝试终止 t 线程");
-        isQuit = true;
+    public static void main(String[] args) {
+        try (OutputStream outputStream = new FileOutputStream("./test.txt", true)) {
+            byte[] buffer = new byte[]{(byte) 0xe4, (byte) 0xbd, (byte) 0xa0, (byte) 0xe5, (byte) 0xe5, (byte) 0xbd};
+            outputStream.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
