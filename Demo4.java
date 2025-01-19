82b8850cd7f2f5a6145009a1a4ead80e15734c24
hulinglingxiaobao <3239197769@qq.com>
2025-01-19 18:19:00 +0800

    2025-1-19 特殊删除

--- Demo4.java
+++ Demo4.java
@@ -1,23 +1,15 @@
-public class Demo4 {
-    public static void main(String[] args) throws InterruptedException {
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    System.out.println("hello Thread");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        t.start();
+import java.io.File;
+import java.util.Scanner;
 
-        while (true) {
-            System.out.println("hello main");
-            Thread.sleep(1000);
-        }
+public class Demo4 {
+    public static void main(String[] args) {
+        // deleteOnExit 在进程退出的时候删除
+        File file = new File("./test.txt");
+        file.deleteOnExit();
+        System.out.println("执行删除完毕.");
+
+        //起到阻塞的作用
+        Scanner sc = new Scanner(System.in);
+        sc.next();
     }
 }
