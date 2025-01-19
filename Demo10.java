115e027030bb1881565bb0ceaf5c68c852d956cc
hulinglingxiaobao <3239197769@qq.com>
2025-01-19 21:11:54 +0800

    2025-1-19 读入

--- Demo10.java
+++ Demo10.java
@@ -4,8 +4,16 @@ import java.io.InputStream;
 
 public class Demo10 {
     public static void main(String[] args) {
-        try (InputStream inputStream = new FileInputStream("./text.txt")) {
-
+        try (InputStream inputStream = new FileInputStream("./test.txt")) {
+            while (true) {
+                int b = inputStream.read();
+                if (b == -1) {
+                    // 读取完毕了
+                    break;
+                }
+                // 表示字节，更习惯使用十六进制打印显示
+                System.out.printf("0x%x\n", b);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
