839bf57ccc2e3c6c996c9dac562e2caaed730a36
hulinglingxiaobao <3239197769@qq.com>
2025-01-19 17:14:23 +0800

    2025-1-19 创建文件

--- Demo2.java
+++ Demo2.java
@@ -1,28 +1,15 @@
-class MyRunnable implements Runnable{
-    @Override
-    public void run() {
-        // 描述了线程要完成的逻辑是啥
-        while(true){
-            System.out.println("hello Thread");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
+import java.io.File;
+import java.io.IOException;
 
-//通过 Runnable 的方式来创建线程
 public class Demo2 {
-    public static void main(String[] args) throws InterruptedException {
-        MyRunnable runnable=new MyRunnable();
-        Thread t=new Thread(runnable);
-        t.start();
+    public static void main(String[] args) throws IOException {
+        File file = new File("./Lisen.js");
 
-        while(true){
-            System.out.println("hello main");
-            Thread.sleep(1000);
-        }
+        boolean ok = file.createNewFile();
+        System.out.println(ok);
+
+        System.out.println(file.exists());
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());
     }
 }
